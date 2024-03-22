52965c5518bedf631322844c9be6720ab029f61f
Afo <obaone01@gmail.com>
2024-03-22 12:17:53 -0700

    initalized with build-onchain-apps

---
+++ web/src/components/layout/header/Experiences.tsx
@@ -0,0 +1,11 @@
+
+import { ListItem } from './ListItem';
+
+export function Experiences() {
+  return (
+    <>
+      <ListItem href="/mint">Mint NFT</ListItem>
+      <ListItem href="/paymaster-bundler">Paymaster Bundler</ListItem>
+    </>
+  );
+}
